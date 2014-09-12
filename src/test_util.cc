db475c8f27bb2eea6be7e1829db66a1bf2025930
Ross Kinder <rkinder@secureworks.com>
2014-09-12 09:19:26 -0400

    be tidy with temporary files used in tests

--- src/test_util.cc
+++ src/test_util.cc
@@ -3,12 +3,53 @@
 // found in the LICENSE file.
 #include "test_util.h"
 
+#define _XOPEN_SOURCE 500
+
 #include <errno.h>
 #include <fcntl.h>
+#include <ftw.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace {
+
+static std::vector<std::string> cleanup_paths;
+
+int RecursiveDeleteCallback(const char *fpath, const struct stat *sb,
+                   int typeflag, struct FTW *ftwbuf) {
+  if (typeflag == FTW_DP) {
+    rmdir(fpath);
+  } else if (typeflag == FTW_F) {
+    unlink(fpath);
+  }
+  return 0;
+}
+
+void RecursiveDelete(const std::string & path) {
+  nftw(path.c_str(), &RecursiveDeleteCallback, 20, FTW_DEPTH|FTW_PHYS);
+}
+
+void DoTempFileCleanup() {
+  for (std::vector<std::string>::const_iterator path = cleanup_paths.begin();
+      path != cleanup_paths.end(); ++path) {
+    RecursiveDelete(*path);
+  }
+}
+
+}  // anonymous namespace
+
+void RemotePathAtExit(const std::string & path) {
+  if (cleanup_paths.empty()) {
+    atexit(&DoTempFileCleanup);
+  }
+  cleanup_paths.push_back(path);
+}
 
 std::string TempDir() {
   char * temp_path = getenv("TEMP");
@@ -19,7 +60,7 @@ std::string TempDir() {
     temp_path = getenv("TMPDIR");
   }
   if (!temp_path) {
-    temp_path = ".";
+    temp_path = "/tmp";
   }
 
   std::string path(temp_path);
@@ -27,6 +68,7 @@ std::string TempDir() {
   if (mkdtemp(const_cast<char *>(path.c_str())) == NULL) {
     return "";
   }
+  RemotePathAtExit(path);
   return path;
 }
 
--- src/test_util.h
+++ src/test_util.h
@@ -6,7 +6,14 @@
 
 #include <string>
 
+// Register `path` to be deleted in an atexit() handler.
+void RemotePathAtExit(const std::string & path);
+
+// Return a new temporary directory which will be deleted when the program
+// exits.
 std::string TempDir();
+
+// Return the contents of `path` as a string.
 std::string GetFileContents(const std::string & path);
 
 #endif  // TEST_UTIL_H_
